5600d1dcc18e0e5d4dd11286195812540253437d
dovadi <frank.oxener@gmail.com>
2013-03-29 10:01:20 +0100

    Set encoding to UTF8

--- lib/parse_p1.rb
+++ lib/parse_p1.rb
@@ -1,5 +1,12 @@
+# encoding: UTF-8
+
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
+if RUBY_VERSION =~ /1.9/
+  Encoding.default_external = Encoding::UTF_8
+  Encoding.default_internal = Encoding::UTF_8
+end
 
 require 'date'
 require 'parse_p1/electricity'
--- lib/parse_p1/electricity.rb
+++ lib/parse_p1/electricity.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 module ParseP1
 
   module Electricity
