4f2472b16a954f49eebdc5f6e820b14425a8b639
dovadi <frank.oxener@gmail.com>
2013-06-15 13:06:43 +0200

    Different OBIS codes for RF Gasmeter now parses correctly

--- test/parse_p1_test.rb
+++ test/parse_p1_test.rb
@@ -7,6 +7,7 @@ class TestParseP1 < Test::Unit::TestCase
   setup do
     #Data string as received on a Ruby on Rails application with some uncleared data in front of it
     @data  = "sBs\u0012C\u0002\u0002\nK*r\"CKR[Wh)\r\n1-0:2.8.1(00000.000*kWh)\r\n1-0:2.8.2(00000.\r\n/ABc1\\1AB123-4567\r\n\r\n0-0:96.1.1(1A123456789012345678901234567890)\r\n1-0:1.8.1(00136.787*kWh)\r\n1-0:1.8.2(00131.849*kWh)\r\n1-0:2.8.1(00002.345*kWh)\r\n1-0:2.8.2(00054.976*kWh)\r\n0-0:96.14.0(0002)\r\n1-0:1.7.0(0003.20*kW)\r\n1-0:2.7.0(0000.12*kW)\r\n0-0:17.0.0(0999.00*kW)\r\n0-0:96.3.10(1)\r\n0-0:96.13.1()\r\n0-0:96.13.0()\r\n0-1:24.1.0(3)\r\n0-1:96.1.0(1234567890123456789012345678901234)\r\n0-1:24.3.0(120502150000)(00)(60)(1)(0-1:24.2.1)(m3)\r\n(00092.112)\r\n0-1:24.4.0(1)\r\n!"
+    # @data = "//ISk5\\2MT382-1003\r\n\r\n0-0:96.1.1(5A424556303035303735343938383131)\r\n1-0:1.8.1(01472.740*kWh)\r\n1-0:1.8.2(01664.635*kWh)\r\n1-0:2.8.1(00000.000*kWh)\r\n1-0:2.8.2(00000.001*kWh)\r\n0-0:96.14.0(0001)\r\n1-0:1.7.0(0000.51*kW)\r\n1-0:2.7.0(0000.00*kW)\r\n0-0:17.0.0(0999.00*kW)\r\n0-0:96.3.10(1)\r\n0-0:96.13.1()\r\n0-0:96.13.0()\r\n0-2:24.1.0(3)\r\n0-2:96.1.0(3338303034303031323138333338363132)\r\n0-2:24.3.0(130615120000)(00)(60)(1)(0-2:24.2.1)(m3)\r\n(02910.805)\r\n0-2:24.4.0(1)\r\n!"
     @p1   = ParseP1::Base.new(@data)
   end
 
@@ -106,6 +107,33 @@ class TestParseP1 < Test::Unit::TestCase
 
     should 'return the total usage of gas' do
       assert_equal 92.112, @p1.gas_usage
+    end
+
+  end
+
+  context 'Gas data send through RF (different OBIS codes)' do
+
+    setup do
+      #Different OBIS codes for RF gas measurement:
+      # => 0-2:24.1.0
+      # => 0-2:96.1.0
+      # => 0-2:24.3.0
+      # => 0-2:24.2.1
+      # => 0-2:24.4.0
+      data  = "sBs\u0012C\u0002\u0002\nK*r\"CKR[Wh)\r\n1-0:2.8.1(00000.000*kWh)\r\n1-0:2.8.2(00000.\r\n/ABc1\\1AB123-4567\r\n\r\n0-0:96.1.1(1A123456789012345678901234567890)\r\n1-0:1.8.1(00136.787*kWh)\r\n1-0:1.8.2(00131.849*kWh)\r\n1-0:2.8.1(00002.345*kWh)\r\n1-0:2.8.2(00054.976*kWh)\r\n0-0:96.14.0(0002)\r\n1-0:1.7.0(0003.20*kW)\r\n1-0:2.7.0(0000.12*kW)\r\n0-0:17.0.0(0999.00*kW)\r\n0-0:96.3.10(1)\r\n0-0:96.13.1()\r\n0-0:96.13.0()\r\n0-2:24.1.0(3)\r\n0-2:96.1.0(1234567890123456789012345678901234)\r\n0-2:24.3.0(120502150000)(00)(60)(1)(0-2:24.2.1)(m3)\r\n(00092.112)\r\n0-2:24.4.0(1)\r\n!"
+      @p2   = ParseP1::Base.new(@data)
+    end
+
+    should 'return the last hourly reading of gas usage' do
+      assert_equal DateTime.new(2012,5,2,15,0), @p2.last_hourly_reading_gas
+    end
+
+    should 'return the measurement unit of gas usage' do
+      assert_equal 'm3', @p2.measurement_unit_gas
+    end
+
+    should 'return the total usage of gas' do
+      assert_equal 92.112, @p2.gas_usage
     end
 
   end
